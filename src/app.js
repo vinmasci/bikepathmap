7358caf7dd43bfbf95bb0a6427984f2a14574a73
vinmasci <vincentmasci@icloud.com>
2024-09-22 08:14:11 +1000

    test

--- src/app.js
+++ src/app.js
@@ -1,10 +1,1 @@
-import mapboxgl from 'mapbox-gl';
-
-mapboxgl.accessToken = 'pk.eyJ1IjoidmlubWFzY2kiLCJhIjoiY20xYmhmempzMG9jdzJpcTN2ZjJ4b3RodyJ9.18y5ZQPBFTIn6mOIcttJnA'; // Replace with your token
-
-const map = new mapboxgl.Map({
-    container: 'app',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [144.9631, -37.8136], // Starting position [longitude, latitude]
-    zoom: 10
-});
+console.log("app.js is successfully loaded.");
