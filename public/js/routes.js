7e7531d7242751d6fa165ff58ad0a04c556ac707
vinmasci <vincentmasci@icloud.com>
2024-10-21 15:37:29 +1100

    Update routes.js

--- public/js/routes.js
+++ public/js/routes.js
@@ -16,7 +16,7 @@ const gravelColors = {
     0: '#01bf11', // Easiest // Green
     1: '#0050c1', // Blue // Intermediate
     2: '#444444', // Diamond // Black
-    3: '#ffffff', // White // Expert 
+    3: '#444444', // White // Expert 
     4: '#FFD43B', // Rail trail // yellow
     5: '#831100' // Red // Closed or Private
 };
