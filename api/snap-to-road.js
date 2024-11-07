5e605a5659b6a20f475623ceb6e5de524e2ab003
vinmasci <vincentmasci@icloud.com>
2024-11-07 16:20:53 +1100

    Update snap-to-road.js

--- api/snap-to-road.js
+++ api/snap-to-road.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     const { points } = req.body;
 
     // Check if points array exists and contains enough data
